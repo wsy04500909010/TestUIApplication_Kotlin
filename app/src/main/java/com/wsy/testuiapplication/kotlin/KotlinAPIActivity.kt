c8e8c2a2b0410615cdf316d53713b4f1cb3cbc0e
wsy04500909010 <niaoniaov@126.com>
2019-12-13 18:19:26 +0800

    add lambda

--- app/src/main/java/com/wsy/testuiapplication/kotlin/KotlinAPIActivity.kt
+++ app/src/main/java/com/wsy/testuiapplication/kotlin/KotlinAPIActivity.kt
@@ -1,11 +1,13 @@
 package com.wsy.testuiapplication.kotlin
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.wsy.testuiapplication.R
 import com.wsy.testuiapplication.kotlin.adapter.ForecastListAdapter
+import com.wsy.testuiapplication.kotlin.bean.Animal
 import com.wsy.testuiapplication.kotlin.bean.ForcastBean
 import kotlinx.android.synthetic.main.activity_kotlin_api.*
 import org.jetbrains.anko.doAsync
@@ -22,6 +24,17 @@ class KotlinAPIActivity : AppCompatActivity() {
 
     var forecastList: MutableList<ModelBean> = ArrayList()
 
+
+    //内联函数  内联函数会把我们调用这个函数的地方替换掉,所以它不需要为此生成一个内部的对象,节省资源
+    //这是一个高阶函数  所谓高阶函数 就是使用一个（子）函数（方法）当做另一个（主）函数的参数或者返回值
+    //这里就是需要传进来一个带有一个Int类型参数的函数作为本函数的 参数，并起个别名为code，通过判断后，通过code（2）执行参数传过来的（子）函数
+
+    inline fun supportsLollipop(code: (Int) -> Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            code(2)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_api)
@@ -29,13 +42,22 @@ class KotlinAPIActivity : AppCompatActivity() {
 //        getDataFromAPI()
 
         rv.layoutManager = LinearLayoutManager(this)
-        rv.adapter = ForecastListAdapter(this, forecastList, object : ForecastListAdapter.OnItemClickListener {
-            override fun invoke(forecast: ModelBean) {
-                Log.d(TAG, forecast.cityName + "===" + forecast.high + "===" + forecast.low)
-            }
-
-        })
+//        rv.adapter = ForecastListAdapter(this, forecastList, object : ForecastListAdapter.OnItemClickListener {
+//            override fun invoke(forecast: ModelBean) {
+//                Log.d(TAG, forecast.cityName + "===" + forecast.high + "===" + forecast.low)
+//            }
+//
+//        })
+        rv.adapter = ForecastListAdapter(this, forecastList) { it ->
+            toast(it.cityName)
+            Log.d(TAG, it.cityName + "===" + it.high + "===" + it.low)
+            66//返回值放最后一行 测试用 无实际意义
+        }
         getDataFromAPItoGson()
+
+        supportsLollipop { number ->
+            toast(number.toString())
+        }
     }
 
 
@@ -76,5 +98,6 @@ class KotlinAPIActivity : AppCompatActivity() {
 
         }
     }
+
 
 }
--- app/src/main/java/com/wsy/testuiapplication/kotlin/adapter/ForecastListAdapter.kt
+++ app/src/main/java/com/wsy/testuiapplication/kotlin/adapter/ForecastListAdapter.kt
@@ -2,6 +2,7 @@ package com.wsy.testuiapplication.kotlin.adapter
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +11,8 @@ import com.wsy.testuiapplication.R
 import com.wsy.testuiapplication.kotlin.bean2.ForcastBean
 import org.jetbrains.anko.find
 
-class ForecastListAdapter(val context: Context, val items: List<ForcastBean>, val listener: OnItemClickListener)
+class ForecastListAdapter(val context: Context, val items: List<ForcastBean>, val listener: (ForcastBean) -> Int)
+//class ForecastListAdapter(val context: Context, val items: List<ForcastBean>, val listener: OnItemClickListener)
     : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
     interface OnItemClickListener {
@@ -34,17 +36,16 @@ class ForecastListAdapter(val context: Context, val items: List<ForcastBean>, va
     }
 
 
-    class ViewHolder(view: View, itemClick: OnItemClickListener) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, val itemClick: (ForcastBean) -> Int) : RecyclerView.ViewHolder(view) {
+        //    class ViewHolder(view: View, val itemClick: OnItemClickListener) : RecyclerView.ViewHolder(view) {
         var date: TextView? = null
         var high: TextView? = null
         var low: TextView? = null
-        var listener: OnItemClickListener? = null
 
         init {
             date = view.find(R.id.date)
             high = view.find(R.id.maxTemperature)
             low = view.find(R.id.minTemperature)
-            listener = itemClick
         }
 
         fun bindForecastData(forecast: ForcastBean) {
@@ -52,7 +53,9 @@ class ForecastListAdapter(val context: Context, val items: List<ForcastBean>, va
             high?.text = forecast.high.toString()
             low?.text = forecast.low.toString()
             itemView.setOnClickListener {
-                listener?.invoke(forecast)
+                //   itemClick?.invoke(forecast)
+                var a: Int = itemClick(forecast)
+                Log.d("ForecastListAdapter", a.toString())
             }
         }
     }
