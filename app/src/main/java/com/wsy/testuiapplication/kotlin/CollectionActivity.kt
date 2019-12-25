b318737d97c7d043fd5ba07b99ed0eddfffda3d6
wsy04500909010 <niaoniaov@126.com>
2019-12-25 18:19:15 +0800

    add collection

--- app/src/main/java/com/wsy/testuiapplication/kotlin/CollectionActivity.kt
+++ app/src/main/java/com/wsy/testuiapplication/kotlin/CollectionActivity.kt
@@ -27,6 +27,12 @@ class CollectionActivity : AppCompatActivity() {
         //dropWhile 返回根据给定函数从第一项开始去掉指定元素的列表。
         Log.d(TAG, list.dropWhile { it < 3 }.size.toString())
 
+        //filter 过滤所有符合给定函数条件的元素
+        Log.d(TAG, list.filter { it % 2 == 0 }.size.toString())
+
+        //filterNot 过滤所有不符合给定函数条件的元素
+        Log.d(TAG, list.filterNot { it < 3 }.size.toString())
+
 
     }
 
