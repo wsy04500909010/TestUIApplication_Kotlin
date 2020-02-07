4d0381cb9ce20b735941e4d307b72af065478c07
wsy <niaoniaov@126.com>
2020-02-07 16:29:59 +0800

    add multi media ad

--- app/src/main/java/com/wsy/testuiapplication/TVBAdActivity.java
+++ app/src/main/java/com/wsy/testuiapplication/TVBAdActivity.java
@@ -14,6 +14,7 @@ import android.widget.VideoView;
 import com.wsy.testuiapplication.ad.bean.AdListBean;
 import com.wsy.testuiapplication.util.ImageUtil;
 import com.wsy.testuiapplication.util.Slog;
+import com.wsy.testuiapplication.util.ToastUtil;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -27,8 +28,9 @@ public class TVBAdActivity extends HXBaseActivity {
 
     private static final String TAG = TVBAdActivity.class.getSimpleName();
 
-    private static final int AD_READY = 1;
-    private static final int AD_ERROR = 0;
+    private static final int AD_READY = 0;
+    private static final int AD_ERROR = 1;
+    private static final int AD_COMPLETE = 2;
 
     private ImageView mAdImage;
     private VideoView mVideoView;
@@ -39,9 +41,9 @@ public class TVBAdActivity extends HXBaseActivity {
 
     //===================广告新逻辑开始
     //    消息类型-请求计时
-    private static final int TYPE_REQUEST_AD_TIME = 2;
+    private static final int TYPE_REQUEST_AD_TIME = 10;
     //    消息类型-图片加载完一张 切换到下一张
-    private static final int TYPE_IMAGE_AD_TIME_COUNT = 3;
+    private static final int TYPE_IMAGE_AD_TIME_COUNT = 11;
 
 
     private static final int TYPE_MEDIAAD_STATUS_NO_RESPONSE = 0;
@@ -61,7 +63,9 @@ public class TVBAdActivity extends HXBaseActivity {
     //    视频广告是否获得响应
     private boolean mMediaAdResponse;
 
-    private int mCurrentAdImageIndex;
+    //下一个图片/视频广告的坐标
+    private int mNextAdImageIndex;
+    private int mNextAdMediaIndex;
 
     // 视频广告等待时间
     private int mAdWaitTime;
@@ -71,8 +75,106 @@ public class TVBAdActivity extends HXBaseActivity {
 //    private LauncherColorListBean.ResultBean.ColorListBean mColor;
 
     private Handler mHandler;
+    private boolean mRunableFlag;//监测卡顿的runnable是否运行
 
     private boolean storagePermission;
+
+    // 老进度 比较老进度和新进度有没有区别 没有区别说明发生了卡顿
+    int old_duration = 0;
+
+
+    Runnable runnable = new Runnable() {
+        public void run() {
+            if (mRunableFlag) {
+                Slog.d(TAG, "waittime=" + mAdWaitTime);
+                int duration = mVideoView.getCurrentPosition();
+                if (old_duration == duration && mVideoView.isPlaying()) {
+
+                    mAdWaitTime++;
+                    Slog.d(TAG, "ad wait=" + mAdWaitTime + "");
+                    if (mAdWaitTime == 10) {
+                        mVideoView.stopPlayback();
+                        startTVBPage();
+                    }
+                } else {
+                    mAdWaitTime = 0;
+                }
+                old_duration = duration;
+
+                mHandler.postDelayed(runnable, 1000);
+            } else {
+                old_duration = 0;//清0 为播放下一段视频广告准备
+            }
+
+        }
+    };
+
+    private void initHandler() {
+
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case AD_ERROR:
+                        mNextAdMediaIndex++;
+                        mRunableFlag = false;
+
+                        if (mNextAdMediaIndex < mMediaAdList.size()) {
+                            mVideoView.setVideoPath(mMediaAdList.get(mNextAdMediaIndex).getUrl());
+                        } else {
+                            //直接跳转
+                            startTVBPage();
+                        }
+                        break;
+                    case AD_READY:
+                        //开始播放视频
+                        mVideoView.start();
+                        mAdImage.setVisibility(View.GONE);
+                        mRunableFlag = true;
+
+                        // 监测是否卡顿
+                        mHandler.postDelayed(runnable, 0);
+                        break;
+                    case AD_COMPLETE:
+                        mNextAdMediaIndex++;
+                        mRunableFlag = false;
+                        if (mNextAdMediaIndex < mMediaAdList.size()) {
+                            mVideoView.setVideoPath(mMediaAdList.get(mNextAdMediaIndex).getUrl());
+                        } else {
+                            startTVBPage();
+                        }
+                        break;
+                    case TYPE_REQUEST_AD_TIME://计时到了5s
+                        if (!mImageAdResponse) {
+                            mImageAdTimeOut = true;
+                            mImageAdFinishFlag = true;
+                            notifyMediaAd();
+                        }
+                        if (!mMediaAdResponse) {
+                            mMediaAdTimeOut = true;
+                            mMediaAdStatus = TYPE_MEDIAAD_STATUS_fail_RESPONSE;
+                            if (mImageAdFinishFlag) {
+                                startTVBPage();
+                            }
+                        }
+                        break;
+                    case TYPE_IMAGE_AD_TIME_COUNT:
+                        mNextAdImageIndex++;
+                        if (mNextAdImageIndex < mImageAdList.size()) {
+                            ImageUtil.showImage(TVBAdActivity.this, mImageAdList.get(mNextAdImageIndex).getUrl(), mAdImage);
+//                            ImageUtil.showImage(TVBAdActivity.this, "http://b.zol-img.com.cn/desk/bizhi/image/5/1920x1200/1409194293467.jpg", mAdImage);
+//                            Glide.with(TVBAdActivity.this).load("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png").into(mAdImage);
+                            mHandler.sendEmptyMessageDelayed(TYPE_IMAGE_AD_TIME_COUNT, Long.parseLong(mImageAdList.get(mNextAdImageIndex).getDuration()));
+                        } else {
+                            imageAdFinish();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+    }
 
 
     @Override
@@ -131,11 +233,11 @@ public class TVBAdActivity extends HXBaseActivity {
         adListBean1.setUrl("http://00.minipic.eastday.com/20170925/20170925141357_d41d8cd98f00b204e9800998ecf8427e_3.jpeg");
         adListBean1.setDuration("3");
         AdListBean adListBean2 = new AdListBean();
-        adListBean1.setUrl("http://img1.imgtn.bdimg.com/it/u=3216941516,4089665709&fm=214&gp=0.jpg");
-        adListBean1.setDuration("3");
+        adListBean2.setUrl("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2026218730,2462529383&fm=11&gp=0.jpg");
+        adListBean2.setDuration("3");
         AdListBean adListBean3 = new AdListBean();
-        adListBean1.setUrl("http://b.zol-img.com.cn/desk/bizhi/image/5/1920x1200/1409194293467.jpg");
-        adListBean1.setDuration("3");
+        adListBean3.setUrl("http://b.zol-img.com.cn/desk/bizhi/image/5/1920x1200/1409194293467.jpg");
+        adListBean3.setDuration("3");
 
         mImageAdList.add(adListBean1);
         mImageAdList.add(adListBean2);
@@ -145,7 +247,7 @@ public class TVBAdActivity extends HXBaseActivity {
         mMediaAdList = new ArrayList<>();
 
         AdListBean adListBean4 = new AdListBean();
-        adListBean4.setUrl("http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4");
+        adListBean4.setUrl("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
         AdListBean adListBean5 = new AdListBean();
         adListBean5.setUrl("http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4");
 
@@ -167,6 +269,8 @@ public class TVBAdActivity extends HXBaseActivity {
             }
             if (bean.getDuration() == null || "".equals(bean.getDuration())) {
                 bean.setDuration(IMAGE_AD_DEFAULT_DISPLAYTIME + "");
+            } else {
+                bean.setDuration(String.valueOf(Integer.valueOf(bean.getDuration()) * 1000));
             }
         }
 
@@ -179,78 +283,6 @@ public class TVBAdActivity extends HXBaseActivity {
         }
 
     }
-
-    private void initHandler() {
-
-        mHandler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case AD_ERROR:
-                        //直接跳转
-                        startTVBPage();
-                        break;
-                    case AD_READY:
-                        //开始播放视频
-                        mVideoView.start();
-                        mAdImage.setVisibility(View.GONE);
-
-                        // 监测是否卡顿
-                        mHandler.postDelayed(runnable, 0);
-                        break;
-                    case TYPE_REQUEST_AD_TIME://计时到了5s
-                        if (!mImageAdResponse) {
-                            mImageAdTimeOut = true;
-                            mImageAdFinishFlag = true;
-                            notifyMediaAd();
-                        }
-                        if (!mMediaAdResponse) {
-                            mMediaAdTimeOut = true;
-                            mMediaAdStatus = TYPE_MEDIAAD_STATUS_fail_RESPONSE;
-                            if (mImageAdFinishFlag) {
-                                startTVBPage();
-                            }
-                        }
-                        break;
-                    case TYPE_IMAGE_AD_TIME_COUNT:
-                        mCurrentAdImageIndex++;
-                        if (mCurrentAdImageIndex < mImageAdList.size()) {
-                            ImageUtil.showImage(TVBAdActivity.this, mImageAdList.get(mCurrentAdImageIndex).getUrl(), mAdImage);
-//                            ImageUtil.showImage(TVBAdActivity.this, "http://b.zol-img.com.cn/desk/bizhi/image/5/1920x1200/1409194293467.jpg", mAdImage);
-
-//                            Glide.with(TVBAdActivity.this).load("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png").into(mAdImage);
-                            mHandler.sendEmptyMessageDelayed(TYPE_IMAGE_AD_TIME_COUNT, Long.parseLong(mImageAdList.get(mCurrentAdImageIndex).getDuration()));
-                        } else {
-                            imageAdFinish();
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            }
-        };
-    }
-
-    int old_duration = 0;
-    Runnable runnable = new Runnable() {
-        public void run() {
-            int duration = mVideoView.getCurrentPosition();
-            if (old_duration == duration && mVideoView.isPlaying()) {
-
-                mAdWaitTime++;
-                Slog.d(TAG, "ad wait=" + mAdWaitTime + "");
-                if (mAdWaitTime == 10) {
-                    mVideoView.stopPlayback();
-                    startTVBPage();
-                }
-            } else {
-                mAdWaitTime = 0;
-            }
-            old_duration = duration;
-
-            mHandler.postDelayed(runnable, 1000);
-        }
-    };
 
 
     private void getIntentData() {
@@ -270,14 +302,13 @@ public class TVBAdActivity extends HXBaseActivity {
         mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                startTVBPage();
+                mHandler.sendEmptyMessage(AD_COMPLETE);
             }
         });
         mVideoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
             @Override
             public boolean onError(MediaPlayer mp, int what, int extra) {
                 Slog.d(TAG, "onError: play error.");
-
                 mHandler.sendEmptyMessage(AD_ERROR);
                 return true;
             }
@@ -305,20 +336,14 @@ public class TVBAdActivity extends HXBaseActivity {
 //        mHandler.sendEmptyMessageDelayed(TYPE_REQUEST_AD_TIME, 5000);
 
         //2、发送图片广告请求
-        requestForImageAd(true);
+//        requestForImageAd(false);
         //3、发送视频广告请求
-        requestForMediaAd(false);
 //        requestForMediaAd(true);
 
 
-        //网络视频
-//        String videoUrl = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
-//        String videoUrl = Environment.getExternalStorageDirectory().getPath() + "/yewen.mp4";
-////        String videoUrl = "http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4";
-//        Uri uri = Uri.parse(videoUrl);
-//        //设置视频路径
-//        mVideoView.setVideoURI(uri);
-
+//        mVideoView.setVideoPath("http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4");
+        mVideoView.setVideoPath("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+//        mVideoView.setVideoPath("http://vjs.zencdn.net/v/oceans.mp4");
 
     }
 
@@ -408,14 +433,21 @@ public class TVBAdActivity extends HXBaseActivity {
 //            mVideoView.setVideoPath(videoUrl);
 //        } else {
 
-        //网络视频
-//        String videoUrl = "https://v-cdn.zjol.com.cn/280443.mp4";
-//        String videoUrl = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
-        String videoUrl = "http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4";
-//        本地视频
-//            String videoUrl = Environment.getExternalStorageDirectory().getPath() + "/yewen.mp4";
-//        //设置视频路径
-        mVideoView.setVideoPath(videoUrl);
+//        //网络视频
+////        String videoUrl = "https://v-cdn.zjol.com.cn/280443.mp4";
+////        String videoUrl = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+//        String videoUrl = "http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4";
+////        本地视频
+////            String videoUrl = Environment.getExternalStorageDirectory().getPath() + "/yewen.mp4";
+////        //设置视频路径
+//        mVideoView.setVideoPath(videoUrl);
+
+        if (mMediaAdList != null && mMediaAdList.size() > 0) {
+            String videoUrl = mMediaAdList.get(0).getUrl();
+            mVideoView.setVideoPath(videoUrl);
+        }
+
+
 //        }
 
     }
@@ -423,6 +455,7 @@ public class TVBAdActivity extends HXBaseActivity {
 
     private void startTVBPage() {
 //        PosterClickUtil.gotoActivityByColorInfo(this, mColor);
+        ToastUtil.showShortToast("这里应该跳转了");
         finish();
     }
 
